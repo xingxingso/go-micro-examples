da41747526e07cd3237518f8a3eca298186356c5
xingxingso <24400338+xingxingso@users.noreply.github.com>
2022-05-20 11:43:40 +0800

    fix

--- config/modify/modify.go
+++ config/modify/modify.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"go-micro.dev/v4/config"
 	"github.com/asim/go-micro/plugins/config/encoder/toml/v4"
-	"go-micro.dev/v4/config/source"
+	"go-micro.dev/v4/config"
+	"go-micro.dev/v4/config/reader"
+	"go-micro.dev/v4/config/reader/json"
 	"go-micro.dev/v4/config/source/file"
 )
 
@@ -16,13 +17,14 @@ func main() {
 
 	// create a new config
 	c, err := config.NewConfig(
+		config.WithReader(json.NewReader(reader.WithEncoder(t))),
 		config.WithSource(
 			// create a new file source
 			file.NewSource(
 				// path of file
-				file.WithPath("./example.conf"),
+				file.WithPath("./example.toml"),
 				// specify the toml encoder
-				source.WithEncoder(t),
+				// source.WithEncoder(t), // not work
 			),
 		),
 	)
@@ -32,7 +34,14 @@ func main() {
 	}
 
 	// load the config
-	if err := c.Load(); err != nil {
+	if err := c.Load(
+		//file.NewSource(
+		//	// path of file
+		//	file.WithPath("./example.toml"),
+		//	// specify the toml encoder
+		//	source.WithEncoder(t),
+		//),
+	); err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -51,10 +60,10 @@ func main() {
 	}
 
 	// write the file
-	if err := os.WriteFile("./example.conf", v, 0644); err != nil {
+	if err := os.WriteFile("./example.toml", v, 0644); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("wrote update to example.conf")
+	fmt.Println("wrote update to example.toml")
 }
